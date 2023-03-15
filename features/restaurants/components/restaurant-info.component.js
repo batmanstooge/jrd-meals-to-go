c67767f9d91dd7494e24ddedf4074189470b83af
batmanstooge <batmanstooge@gmail.com>
2023-03-15 16:31:31 +0530

    Added bare data to Restaurant Info component

--- features/restaurants/components/restaurant-info.component.js
+++ features/restaurants/components/restaurant-info.component.js
@@ -1,3 +1,16 @@
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => <Text>Restaurant Info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Regina Cassandra",
+    icon,
+    photos = [
+      "https://upload.wikimedia.org/wikipedia/commons/4/4b/Regina_Cassandra_grace_the_launch_of_Aankhen_2_%2817%29_%28cropped%29.jpg",
+    ],
+    address = "Madras, TamilNadu",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
+};
