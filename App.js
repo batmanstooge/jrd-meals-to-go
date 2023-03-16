407bc2f5b93640b11ca871f6397cd2a5ff858197
batmanstooge <batmanstooge@gmail.com>
2023-03-16 21:34:15 +0530

    Optimized tabBarIcon

--- App.js
+++ App.js
@@ -17,10 +17,28 @@ import {
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Settings = () => <Text>Setting</Text>;
 
 const Map = () => <Text>Map</Text>;
+
+const getIconName = (route, focused) => {
+  let iconName;
+  if (route.name === "restaurants") {
+    iconName = focused ? "md-restaurant" : "md-restaurant-outline";
+  } else if (route.name === "map") {
+    iconName = focused ? "md-map" : "md-map-outline";
+  } else if (route.name === "settings") {
+    iconName = focused ? "md-settings" : "md-settings-outline";
+  }
+  return iconName;
+};
+
+const getTabBarIcon = (route, focused, color, size) => {
+  const iconName = getIconName(route, focused);
+  return <Ionicons name={iconName} size={size} color={color} />;
+};
 
 const Tab = createBottomTabNavigator();
 
@@ -40,7 +58,15 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                return getTabBarIcon(route, focused, color, size);
+              },
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+            })}
+          >
             <Tab.Screen component={RestaurantsScreen} name="restaurants" />
             <Tab.Screen component={Map} name="map" />
             <Tab.Screen component={Settings} name="settings" />
