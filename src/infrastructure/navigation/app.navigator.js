3f44a5e9664da39d9447b60a669110eb7c58d43e
batmanstooge <batmanstooge@gmail.com>
2023-03-18 12:02:25 +0530

    ch164: Restructuring Navigation

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,43 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+const Tab = createBottomTabNavigator();
+const Settings = () => <Text>Setting</Text>;
+const Map = () => <Text>Map</Text>;
+const getIconName = (route, focused) => {
+  let iconName;
+  if (route.name === "restaurants") {
+    iconName = focused ? "md-restaurant" : "md-restaurant-outline";
+  } else if (route.name === "map") {
+    iconName = focused ? "md-map" : "md-map-outline";
+  } else if (route.name === "settings") {
+    iconName = focused ? "md-settings" : "md-settings-outline";
+  }
+  return iconName;
+};
+
+const getTabBarIcon = (route, focused, color, size) => {
+  const iconName = getIconName(route, focused);
+  return <Ionicons name={iconName} size={size} color={color} />;
+};
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            return getTabBarIcon(route, focused, color, size);
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen component={RestaurantsScreen} name="restaurants" />
+        <Tab.Screen component={Map} name="map" />
+        <Tab.Screen component={Settings} name="settings" />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
