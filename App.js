951eaedb949704cf25492cb50852bc1e4ce30e1a
batmanstooge <batmanstooge@gmail.com>
2023-03-19 22:49:37 +0530

    ch190: Firebase Installation

--- App.js
+++ App.js
@@ -17,6 +17,19 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUWq6Mhg3roLvyctxPMuRgKNTFZr9HPxg",
+  authDomain: "jrd-complete-react-native-2023.firebaseapp.com",
+  projectId: "jrd-complete-react-native-2023",
+  storageBucket: "jrd-complete-react-native-2023.appspot.com",
+  messagingSenderId: "427867086723",
+  appId: "1:427867086723:web:1aac39f8af6440cba5a0b6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useFontsOswald({
