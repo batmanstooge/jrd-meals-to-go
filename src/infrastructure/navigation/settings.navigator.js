69c8f5aed438f289281a03af677fcf1931be7c82
batmanstooge <batmanstooge@gmail.com>
2023-03-22 11:28:22 +0530

    ch210: Favourites Screen Preliminaries

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -4,6 +4,7 @@ import {
   createStackNavigator,
   CardStyleInterpolators,
 } from "@react-navigation/stack";
+import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 
 const SettingsStack = createStackNavigator();
 
@@ -22,7 +23,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         name="Settings"
         component={SettingsScreen}
       />
-      <SettingsStack.Screen name="Favourites" component={() => null} />
+      <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
     </SettingsStack.Navigator>
   );
 };
